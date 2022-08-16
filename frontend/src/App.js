14548bfabfe33b8569110e4bc33cda8f095198f7
Dekopossas <andrepossaster@gmail.com>
2022-08-15 23:00:46 -0300

    scope done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Form } from '@unform/web';
+import { Scope } from '@unform/core';
+
 import Input from './components/Form/Input';
 
 const initialData = {
   email: 'andre@possas.com',
-}
+};
 
 function App() {
   const handleSubmit = (data) => {
@@ -16,9 +18,17 @@ function App() {
       <h1>hellow world</h1>
 
       <Form initialData={initialData} onSubmit={handleSubmit}>
-          <Input name="name" />
-          <Input type="email" name="email" />
-          <Input type="password" name="passaword" />
+        <Input name="name" placeholder="nome" />
+        <Input type="email" name="email" placeholder="email" />
+        <Input type="password" name="passaword" placeholder="senha" />
+
+        <Scope path="address">
+          <Input name="street" placeholder="rua"/>
+          <Input name="neighborhood" placeholder="bairro"/>
+          <Input name="city" placeholder="cidade"/>
+          <Input name="state" placeholder="estado"/>
+          <Input name="number" placeholder="numero"/>
+        </Scope>
 
         <button type="submit">Enviar</button>
       </Form>
